648d66c603a8b5f280df9878eacc90ede30307be
Corey Mann <corey.mann@variableinc.com>
2021-07-14 20:07:19 -0400

    adds cursor css styling

--- src/components/Hoverable.js
+++ src/components/Hoverable.js
@@ -2,11 +2,14 @@ import * as React from 'react'
 
 
 export default function HoverContainer({children}) {
-  const [isHovering, setIsHovering] = React.useState<boolean>(false);
+  const [isHovering, setIsHovering] = React.useState(false);
   return <div  
     onMouseEnter={() => setIsHovering(true)} 
     onMouseLeave={() => setIsHovering(false)} 
-    style={{ backgroundColor: isHovering ? 'rgba(0,0,255,0.3)' : undefined }}  
+    style={{ 
+      backgroundColor: isHovering ? 'rgba(0,0,255,0.3)' : undefined,
+      cursor: isHovering ? 'pointer' : undefined
+    }}  
   >
     {children} 
   </div>
